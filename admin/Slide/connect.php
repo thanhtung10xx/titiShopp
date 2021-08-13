c2ff6f48965a4daf342aaa72f4eb4035abebb5a9
Your Name <you@example.com>
2021-08-13 18:16:00 +0700

    update

--- admin/Slide/connect.php
+++ admin/Slide/connect.php
@@ -1,6 +1,7 @@
 <?php
-$connect = mysqli_connect('localhost','root','','titishop');
+$connect = mysqli_connect('18.217.238.58','tungne','123@123','titishop');
+mysqli_set_charset($connect, 'UTF8');
 if (!$connect){
-echo "ket noi that bai";
+    echo "ket noi that bai";
 }
 ?>
