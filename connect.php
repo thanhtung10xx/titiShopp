44363ebce67f754db444fdcac57dae40f8eb3141
Your Name <you@example.com>
2021-08-13 16:12:49 +0700

    tung

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect('18.217.238.58','titi','123@123a','titishop');
+$connect = mysqli_connect('localhost','root','','titishop');
 if (!$connect){
 echo "ket noi that bai";
 }
