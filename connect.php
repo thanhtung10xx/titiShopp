a070b696639c5038bea235a946f0773a21364d50
Your Name <you@example.com>
2021-08-15 17:39:59 +0700

    done

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect('3.141.42.181','tungne','123@123','titishop');
+$connect = mysqli_connect('18.119.112.16','tungne','123@123','titishop');
 mysqli_set_charset($connect, 'UTF8');
 if (!$connect){
     echo "ket noi that bai";
